722f89e2a6ea751165e0e0750d792c6c51b9e480
Best\Sorathorn <sorathorn.kh@mail.kmutt.ac.th>
2021-03-28 15:09:48 +0700

    krn

---
+++ exam2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace xyz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, y, krn;
+            Console.Write("Input a number: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Input b number: ");
+            b = int.Parse(Console.ReadLine());
+            x = a;
+            y = b;
+
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+            }
+            krn = (x * y) / a;
+            Console.Write("krn: " + krn);
+            Console.Read();
+        }
+    }
+}
